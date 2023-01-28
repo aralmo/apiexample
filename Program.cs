418232b4b8b65f6ac059f4edfee465b072f99fae
Aridane Alamo <aridane.alamo@col.vueling.com>
2023-01-28 10:12:04 +0100

    add open telemetry

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Instrumentation.Http;
+using OpenTelemetry.Trace;
+
+var serviceName = "WeatherAPI";
+var serviceVersion = "1.0.0";
+
 var builder = WebApplication.CreateBuilder(args);
 
 //dependency injection
 builder.Services.AddHandler<RandomWeatherForecastHandler>();
 builder.Services.AddMediator();
+
+//opentelemetry
+builder.Services
+    .AddOpenTelemetry()
+    .WithTracing(options =>
+    {
+        options
+            .AddSource(serviceName)
+            .AddConsoleExporter()
+            .SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation(options => { });
+    })
+    .StartWithHost();
+    
 
 var app = builder.Build();
 
